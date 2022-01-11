ae00e1c4abc7ad971d56a0c84188ab5b7733dd67
Esteban Cabrera Gómez <ecabrera@hostyweb.cl>
2022-01-11 16:31:46 -0300

    sin comentarios

--- src/api/fetchItems.ts
+++ src/api/fetchItems.ts
@@ -2,7 +2,7 @@
 import { IItem } from '../redux/cartSlice';
 import { cartItems } from '../__mock__/cartItems';
 
-const delay = 1000; //1s
+const delay = 1000;
 
 export const fetchItems = () => {
   return new Promise<IItem[]>((resolve) => {
